1ae2e631fd0de83a21e421210540d9a0aa06d953
Sebastian Vera <jj.sebas.v@gmail.com>
2016-09-14 20:48:13 -0300

    Update schema

--- apuntes-webapp/persistance/src/main/resources/schema.sql
+++ apuntes-webapp/persistance/src/main/resources/schema.sql
@@ -1,3 +1,12 @@
+CREATE TABLE IF NOT EXISTS files (
+	fileid SERIAL PRIMARY KEY,
+	username varchar(100),
+	university varchar(100),
+	career varchar(100),
+	subject varchar(100),
+	uploaded_file bytea
+);
+
 CREATE TABLE IF NOT EXISTS users (
 	userid SERIAL PRIMARY KEY,
 	username varchar(100),
