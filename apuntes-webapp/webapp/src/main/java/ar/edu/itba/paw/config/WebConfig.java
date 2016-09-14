c4d29bade7ef85b653cfed24aeaa5202e85a37ad
Sebastian Vera <jj.sebas.v@gmail.com>
2016-09-14 19:50:19 -0300

    sql schema

--- apuntes-webapp/webapp/src/main/java/ar/edu/itba/paw/config/WebConfig.java
+++ apuntes-webapp/webapp/src/main/java/ar/edu/itba/paw/config/WebConfig.java
@@ -2,10 +2,15 @@ package ar.edu.itba.paw.config;
 
 import javax.sql.DataSource;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+import org.springframework.jdbc.datasource.init.DataSourceInitializer;
+import org.springframework.jdbc.datasource.init.DatabasePopulator;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -18,6 +23,23 @@ import org.springframework.web.servlet.view.JstlView;
 @Configuration
 public class WebConfig extends WebMvcConfigurerAdapter{
 
+	@Value("classpath:schema.sql")
+	private Resource schemaSql;
+	
+	@Bean
+	public DataSourceInitializer dataSourceInitializer(final DataSource ds) {
+		final DataSourceInitializer dsi = new DataSourceInitializer();
+		dsi.setDataSource(ds);
+		dsi.setDatabasePopulator(databasePopulator());
+		return dsi;
+	}
+	
+	private DatabasePopulator databasePopulator() {
+		final ResourceDatabasePopulator dbp = new ResourceDatabasePopulator();
+		dbp.addScript(schemaSql);
+		return dbp;
+	}
+	
 	@Bean
 	public ViewResolver viewResolver() {
 		final InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
@@ -43,7 +65,5 @@ public class WebConfig extends WebMvcConfigurerAdapter{
 		// TODO Auto-generated method stub
 		registry.addResourceHandler("/css/**").addResourceLocations("/css/");
 	}
-	
-	
 	
 }
