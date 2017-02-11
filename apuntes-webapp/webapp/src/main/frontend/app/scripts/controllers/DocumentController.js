dc6d81dc2204dfa1f3152dc98ce7b26c54760a99
heyimnowi <noelia.lopez@mercadolibre.com>
2017-02-11 18:11:56 -0300

    Document service and controller. Deleting analytics code

--- apuntes-webapp/webapp/src/main/frontend/app/scripts/controllers/DocumentController.js
+++ apuntes-webapp/webapp/src/main/frontend/app/scripts/controllers/DocumentController.js
@@ -1,8 +1,16 @@
 'use strict';
 define(['frontend'], function(frontend) {
 
-    frontend.controller('DocumentController', function($scope) {
+    frontend.controller('DocumentController',
+      function(documentService, $routeParams) {
+        var _this = this;
+        documentService.getCourse($routeParams.documentId).then(function(result) {
+          _this.document = result.data;
+        });
 
+        documentService.getComments($routeParams.documentId).then(function(result) {
+          _this.comments = result.data;
+        });
     });
 
 });
