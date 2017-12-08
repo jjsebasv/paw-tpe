0069b2b715490c84a4b87bdcf47eb9f6d714d1f9
Sebastian Vera <jj.sebas.v@gmail.com>
2017-12-08 16:10:33 -0300

    error modal

--- apuntes-webapp/webapp/src/main/frontend/app/scripts/directives/errormodalDirective.js
+++ apuntes-webapp/webapp/src/main/frontend/app/scripts/directives/errormodalDirective.js
@@ -1,14 +1,19 @@
 'use strict';
 
 define(['frontend', 'services/errormodalService'], function(frontend) {
-    frontend.directive('errormodal', function() {
+    frontend.directive('errormodal', function(errormodalService) {
         return {
             restrict: 'E',
             scope: {
               errors: '='
             },
             replace: false,
-            templateUrl: '../../views/_errormodalDirective.html'
+            templateUrl: '../../views/_errormodalDirective.html',
+            link: function($scope, element, attrs) {
+              $scope.closeModal = function(){
+                errormodalService.hideErrorModal();
+              }
+            }
         };
     });
     return frontend;
